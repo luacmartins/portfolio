cc09e997075f1e3e643d899c4aa949762391dc13
Lua Carlos Martins <luacmartins@Luas-MacBook-Pro.local>
2020-10-08 13:01:17 -0600

    add title tag

--- pages/bio/bio.md
+++ pages/bio/bio.md
@@ -2,5 +2,5 @@
 profilePhoto: '/images/profile.jpg'
 profilePhotoAlt: 'profile picture'
 heading: "Hi, my name is Carlos. I'm a Software Engineer."
-text: "I'm a passionate software engineer using cutting edge technology to create real-world impact. I am currently looking for remote software engineer opportunities to help companies reach their goals. Previously, I was the CEO of an imports and exports company in Brazil where I restructued operations that led to multimillion dollar profits. I am also passionate about travelling and photography."
+text: "I'm a passionate software engineer using cutting edge technology to create real-world impact. I am currently looking for remote software engineer opportunities to help companies reach their goals. Previously, I was the CEO of an imports and exports company in Brazil where I restructued operations that led to multimillion dollar profits. I am also passionate about traveling and photography."
 ---
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import SocialBar from '../components/SocialBar'
 import List from '../components/List'
 // import { getSortedProjectsData, getBioData } from '../lib/fetchData'
@@ -5,36 +6,41 @@ import { getSortedData } from '../lib/fetchData'
 
 export default function IndexPage({ allProjectsData, bioData, allExperienceData, allEducationData }) {
    return (
-      <div className="min-h-screen py-16 px-4 text-gray-800 lg:px-0 lg:py-0">
-         <div className="flex items-center justify-center sm:px-24 lg:justify-start lg:pt-16 text-center flex-col lg:fixed lg:px-16 lg:w-104">
-            <img className="rounded-full w-48 shadow-lg" src={bioData.profilePhoto} alt={bioData.profilePhotoAlt} />
-            <h2 className="mt-8 text-2xl leading-tight font-thin">{bioData.heading}</h2>
-            <div className="mt-4">
-               <SocialBar />
+      <>
+         <Head>
+            <title>Carlos Martins Portfolio</title>
+         </Head>
+         <div className="min-h-screen py-16 px-4 text-gray-800 lg:px-0 lg:py-0">
+            <div className="flex items-center justify-center sm:px-24 lg:justify-start lg:pt-16 text-center flex-col lg:fixed lg:px-16 lg:w-104">
+               <img className="rounded-full w-48 shadow-lg" src={bioData.profilePhoto} alt={bioData.profilePhotoAlt} />
+               <h2 className="mt-8 text-2xl leading-tight font-thin">{bioData.heading}</h2>
+               <div className="mt-4">
+                  <SocialBar />
+               </div>
+            </div>
+            <div className="mt-12 sm:px-24 lg:px-0 lg:pt-16 lg:mt-0 lg:w-1/2 lg:ml-104 lg:pb-16">
+               <p className="font-thin text-lg">{bioData.text}</p>
+
+               {/* Experience */}
+               <section className="mt-16">
+                  <header className="font-semibold text-3xl mt-6 border-b">Experience</header>
+                  <List data={allExperienceData} />
+               </section>
+
+               {/* Projects */}
+               <section className="mt-16">
+                  <header className="font-semibold text-3xl mt-6 border-b">Projects</header>
+                  <List data={allProjectsData} card='project' />
+               </section>
+
+               {/* Education */}
+               <section className="mt-16">
+                  <header className="font-semibold text-3xl mt-6 border-b">Education</header>
+                  <List data={allEducationData} />
+               </section>
             </div>
          </div>
-         <div className="mt-12 sm:px-24 lg:px-0 lg:pt-16 lg:mt-0 lg:w-1/2 lg:ml-104 lg:pb-16">
-            <p className="font-thin text-lg">{bioData.text}</p>
-
-            {/* Experience */}
-            <section className="mt-16">
-               <header className="font-semibold text-3xl mt-6 border-b">Experience</header>
-               <List data={allExperienceData} />
-            </section>
-
-            {/* Projects */}
-            <section className="mt-16">
-               <header className="font-semibold text-3xl mt-6 border-b">Projects</header>
-               <List data={allProjectsData} card='project' />
-            </section>
-
-            {/* Education */}
-            <section className="mt-16">
-               <header className="font-semibold text-3xl mt-6 border-b">Education</header>
-               <List data={allEducationData} />
-            </section>
-         </div>
-      </div>
+      </>
    )
 }
 
